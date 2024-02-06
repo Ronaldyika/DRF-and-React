199659d3e5b95f05f3b5f84cdf5f978db3bcfd00
Ronaldyika <buhnyuyronald@gmail.com>
2024-02-06 18:47:53 +0100

    done with the authorisation classes

--- watchmate/user_app/api/serializers.py
+++ watchmate/user_app/api/serializers.py
@@ -2,28 +2,28 @@ from rest_framework import serializers
 from django.contrib.auth.models import User 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    # password2  = serializers.CharField(style={'input_type':'password'},write_only = True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
 
     class Meta:
         model = User
-        # fields = ['username','email','password','password2']
-        fields = ['username','email','password']
+        fields = ['username', 'email', 'password', 'password2']
 
-        # extra_kwargs ={
-        #     'password2':{'write_only':True}
-        # }
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
-        def save(self):
-            password = self.validated_data['password']
-            # password2 = self.validated_data['password2']
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
 
-            # if password != password2:
-            #     raise serializers.ValidationError({'error': 'password1 and password2 should besame'})
-            if User.objects.filter(email=self.validated_data['email']).exists():
-                raise serializers.ValidationError({'error':'email already exists'})
-            
-            account = User(email=self.validated_data['email'],username=self.validated_data['username'])
-            account.set_password(password)
-            account.save()
+        if password != password2:
+            raise serializers.ValidationError({'error': 'password1 and password2 should be the same'})
 
-            return account
+        if User.objects.filter(email=self.validated_data['email']).exists():
+            raise serializers.ValidationError({'error': 'email already exists'})
+
+        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        account.set_password(password)
+        account.save()
+
+        return account
