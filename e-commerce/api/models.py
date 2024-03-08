ae9112ec5f2a20bf7fe1b2772168a30c62d18eb6
Ronaldyika <buhnyuyronald@gmail.com>
2024-03-09 00:52:41 +0100

    added the gitignore firle

--- e-commerce/api/models.py
+++ e-commerce/api/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,Group, Permission
 
-class Customer(AbstractUser):
-    pass
+class Customer(models.Model):
+    groups = models.ManyToManyField(Group, related_name='customer_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='customer_user_permissions')
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price= models.DecimalField(max_digit = 10,decimal_places = 2)
+    price= models.DecimalField(max_digits = 10,decimal_places = 2)
 
     def __str__(self):
         return self.name
--- e-commerce/ecommerce/settings.py
+++ e-commerce/ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        # ... other authentication classes ...
+    ],
+    # ... other REST framework settings ...
+}
 
 ROOT_URLCONF = 'ecommerce.urls'
 
